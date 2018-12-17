a5ea89db80ab77341e0e038f01cf23389f232243
Marcelo Pilatti <marcelo.silva979@gmail.com>
2018-12-17 10:47:31 -0200

    .

--- src/Validator.php
+++ src/Validator.php
@@ -507,7 +507,7 @@ class Validator
         return $result;
     }
 
-    public static function validateFile(UploadedFile $file, $validExtensions)
+    public static function validateFile($file, $validExtensions)
     {
         try {
             $error = null;
